7f3b795916bfb60eb0c7823630c08f26dc734a64
SarahNk1-Maker <sarahnaveed216@gmail.com>
2024-08-31 01:45:58 -0400

    vercel debugging

--- middleware.js
+++ middleware.js
@@ -1,14 +1,12 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// Use Clerk middleware for authentication
-export default clerkMiddleware();
+export default clerkMiddleware({
+  apiKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+});
 
-// Define configuration for the middleware
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
