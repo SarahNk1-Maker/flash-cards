15cd9262fa8e00654fd71f5569f825d87523fd1b
SarahNk1-Maker <sarahnaveed216@gmail.com>
2024-09-02 14:08:02 -0400

    vercel debugging

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ClerkProvider frontendApi={process.env.NEXT_PUBLIC_CLERK_CLERK_PUBLISHABLE_KEY}>
+        <ClerkProvider frontendApi={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
           {children}
         </ClerkProvider>
       </body>
