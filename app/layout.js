cfe5ab0a385284a32d60b94a02c9bf3af1c95750
SarahNk1-Maker <sarahnaveed216@gmail.com>
2024-09-02 13:42:59 -0400

    vercel debugging

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+// app/layout.js
 import { ClerkProvider } from '@clerk/nextjs';
 import { Inter } from 'next/font/google';
 import './globals.css'; // Global styles if needed
@@ -8,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ClerkProvider>
+        <ClerkProvider frontendApi={process.env.NEXT_PUBLIC_CLERK_CLERK_PUBLISHABLE_KEY}>
           {children}
         </ClerkProvider>
       </body>
